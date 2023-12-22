199fa822fd78efe75bb4c249fa1ed3d134b6af8f
Nafunnn <aanalim31@gmail.com>
2023-12-22 19:36:20 +0700

    [+] add new feature & redesign

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from openai import OpenAI
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 
@@ -12,9 +13,14 @@ client = OpenAI(api_key=api_key)
 
 chat_history = []
 
-def chat_with_gpt(prompt):
+def get_dynamic_system_message(user_input):
+    # Your logic to generate or retrieve the dynamic system message based on user input
+    # For now, it's a placeholder
+    return f"Dynamic system message for user input: {user_input}"
+
+def chat_with_gpt(prompt, system_message):
     messages = [
-        {"role": "system", "content": "kamu adalah penulis konten yang hebat. buatkan saya tulisan menjadi 4 bagian untuk instagram saya. 1 pendahuluan, 2 latar belakang, 3 masalah 4 solusi. kamu akan saya berikan topik sebagai acuan."},
+        {"role": "system", "content": system_message},
         {"role": "user", "content": prompt}
     ]
 
@@ -29,13 +35,32 @@ def chat_with_gpt(prompt):
 def index():
     if request.method == "POST":
         user_message = request.form["user_message"]
-        chat_history.append({"role": "user", "content": user_message})
+        chat_history.append({
+            "role": "user",
+            "content": user_message,
+            "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            })
+
+        # Dynamically get the system message based on user input
+        system_message = get_dynamic_system_message(user_message)
 
         # Get GPT-3.5 Turbo's response
-        gpt_response = chat_with_gpt(user_message)
-        chat_history.append({"role": "assistant", "content": gpt_response})
+        gpt_response = chat_with_gpt(user_message, system_message)
+        chat_history.append({
+            "role": "assistant",
+            "content": gpt_response,
+            "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            })
 
     return render_template("index.html", chat_history=chat_history)
+
+@app.route("/clear", methods=["POST"])
+def clear_chat():
+    global chat_history
+    chat_history = []
+    print("Chat history cleared")
+    return redirect(url_for("index"))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
